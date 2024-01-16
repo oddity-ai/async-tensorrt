d21ae988731a4d83122e5c5bc167785e65809476
Gerwin van der Lugt <account+git@gerwin3.com>
2024-01-16 13:48:26 +0100

    twin pr for https://github.com/oddity-ai/async-cuda/pull/4

--- build.rs
+++ build.rs
@@ -1,26 +1,38 @@
-#[cfg(unix)]
 fn main() {
-    cpp_build::Config::new()
-        .include("/usr/local/cuda/include")
-        .include("/usr/local/tensorrt/include")
-        .build("src/lib.rs");
-    println!("cargo:rustc-link-search=/usr/local/cuda/lib64");
-    println!("cargo:rustc-link-search=/usr/local/tensorrt/lib64");
-    println!("cargo:rustc-link-lib=nvinfer");
-    println!("cargo:rustc-link-lib=nvonnxparser");
-}
+    let cuda_path = std::env::var("CUDA_PATH").map(std::path::PathBuf::from);
 
-#[cfg(windows)]
-fn main() {
-    let cuda_path = std::env::var("CUDA_PATH").expect("Missing environment variable `CUDA_PATH`.");
-    let cuda_path = std::path::Path::new(&cuda_path);
-    cpp_build::Config::new()
-        .include(cuda_path.join("include"))
-        .build("src/lib.rs");
-    println!(
-        "cargo:rustc-link-search={}",
-        cuda_path.join("lib").join("x64").display()
-    );
+    #[cfg(not(windows))]
+    let cuda_path = cuda_path.unwrap_or_else(|_| std::path::PathBuf::from("/usr/local/cuda"));
+    #[cfg(windows)]
+    let cuda_path = cuda_path.expect("Missing environment variable `CUDA_PATH`.");
+
+    let cuda_include_path = std::env::var("CUDA_INCLUDE_PATH")
+        .map(std::path::PathBuf::from)
+        .unwrap_or_else(|_| cuda_path.join("include"));
+
+    let cuda_lib_path = std::env::var("CUDA_LIB_PATH")
+        .map(std::path::PathBuf::from)
+        .unwrap_or_else(|_| {
+            #[cfg(not(windows))]
+            {
+                cuda_path.join("lib64")
+            }
+            #[cfg(windows)]
+            {
+                cuda_path.join("lib").join("x64")
+            }
+        });
+
+    let mut cpp_build_config = cpp_build::Config::new();
+    cpp_build_config.include(cuda_include_path);
+    #[cfg(not(windows))]
+    cpp_build_config.include("/usr/local/tensorrt/include");
+    cpp_build_config.build("src/lib.rs");
+
+    println!("cargo:rustc-link-search={}", cuda_lib_path.display());
+    #[cfg(not(windows))]
+    println!("cargo:rustc-link-search=/usr/local/tensorrt/lib64");
+
     println!("cargo:rustc-link-lib=nvinfer");
     println!("cargo:rustc-link-lib=nvonnxparser");
 }
